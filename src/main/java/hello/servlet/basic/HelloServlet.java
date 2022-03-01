6b3cb9ecc787179ae9f108efb7b93a3276c03b66
yeonha99 <rhdqn1207@naver.com>
2022-03-01 14:05:52 +0900

    feat:hello servlet

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,32 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/*
+    HttpServlet을 상속받아야함
+    컨트롤+o 하면 검색해서 호출 가능
+    soutm 호출 함수 이름 출력
+    soutv 변수 출력
+ */
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username=request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello "+username); //응답 데이터 넣기
+
+    }
+}
